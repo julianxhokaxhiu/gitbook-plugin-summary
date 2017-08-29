b0c3ab5a20aec2119d725bc7b810e2772ed4f88b
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-08-29 10:21:35 +0200

    Refactor the whole plugin
    Custom made, with only one dependency ( glob ) and fully using configurations from gitbook book.json. No custom configurations needed.

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-let summary = require( 'gitbook-summary/lib/summary' )
+const fs = require('fs'),
+      glob = require('glob'),
+      jsonMark = require('jsonmark'),
+      path = require('path')
+
+function generateEntry( title, path, readmeFilename ) {
+  let depth = path.match(/\//g).length
+
+  if ( path.indexOf( readmeFilename ) == -1 )
+    depth++
+
+  return `${Array(depth).join('    ')}- [${title}](${path})\n`
+}
 
 module.exports = {
   hooks: {
     init: function () {
-      summary({
-        root: this.resolve('')
-      })
+      const root = this.resolve(''),
+            bookTitle = this.config.get('title'),
+            readmeFilename = this.config.get('structure.readme'),
+            summaryFilename = this.config.get('structure.summary'),
+            opts = this.config.get('pluginsConfig.summary')
+
+      glob(
+        `*/**/*.md`,
+        {
+          cwd: root
+        },
+        ( err, files ) => {
+          let summaryContent = ( bookTitle ? `# ${bookTitle}\n\n` : '' )
+
+          files.forEach( ( filePath ) => {
+            const markdown = jsonMark.parse( fs.readFileSync( `${root}/${filePath}`, { encoding: 'utf8' } ) ),
+                  fileTitle = markdown.order[0]
+
+            summaryContent += generateEntry( fileTitle, filePath, readmeFilename )
+          })
+
+          fs.writeFileSync( `${root}/${summaryFilename}`, summaryContent, { encoding: 'utf8' } )
+        }
+      )
     }
   }
 }
