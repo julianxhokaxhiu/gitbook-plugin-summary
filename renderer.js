35e645f84363b6b23db5d74d0240fdc9a1274a70
Julien Deniau <julien.deniau@gmail.com>
2018-03-16 11:22:16 +0100

    Use filename if there is no title in the Markdown file

--- renderer.js
+++ renderer.js
@@ -20,12 +20,18 @@ const getDirTitle = path =>
     .chain(x => Maybe.fromNullable(x[x.length - 2]))
     .getOrElse('NO_NAME') // shouldn't happen, right !?
 
+const getFileName = path =>
+  Maybe.of(path.split('/'))
+    .chain(x => Maybe.fromNullable(x[x.length - 1]))
+    .getOrElse('NO_NAME') // shouldn't happen, right !?
+
 // (String -> Bool) -> [ String, Markdown ] -> String
 const fileEntry = isReadme => ([ filePath, parsedMarkdown ]) => {
   if (isReadme(filePath)) return
 
   const depth = getFileDepth(filePath)
-  const fileTitle = getFileTitle(parsedMarkdown)
+  console.log(filePath, parsedMarkdown);
+  const fileTitle = getFileTitle(parsedMarkdown) || getFileName(filePath)
 
   return linkEntries(depth, fileTitle, filePath)
 }
@@ -34,8 +40,9 @@ const getFileTitle = parsedMarkdown =>
   parsedMarkdown
     .chain(m => Maybe.fromNullable(m.headings))
     .map(headings => headings[0])
+    .filter(title => !!title)
     .map(title => title.trim())
-    .getOrElse('NO_NAME')
+    .getOrElse(null)
 
 const depthEntries = (depth, entries) =>
   Array(depth).join('    ') + entries
