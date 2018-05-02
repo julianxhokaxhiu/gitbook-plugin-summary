f075c22d4a7778520dfc9164c3de8ce841c2245c
Marco Jorge <marco.jorge@feedzai.com>
2018-05-03 00:37:01 +0800

    Ignore the styles folder

--- fs.js
+++ fs.js
@@ -41,7 +41,7 @@ const getPaths = (config, pattern) =>
     pattern,
     {
       cwd: config.root,
-      ignore: [ 'node_modules/**', '_book' ],
+      ignore: [ 'node_modules/**', '_book', 'styles' ],
       nosort: true
     }
   )
