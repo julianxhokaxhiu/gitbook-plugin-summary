fb5b7cb701233d282f5f7cf341969624a6104b77
Marco Jorge <marco.jorge@feedzai.com>
2018-05-11 22:53:15 -0400

    Ignore img folders

--- fs.js
+++ fs.js
@@ -41,7 +41,7 @@ const getPaths = (config, pattern) =>
     pattern,
     {
       cwd: config.root,
-      ignore: [ 'node_modules/**', '_book', 'styles', '_layouts', '_layouts/website' ],
+      ignore: [ 'node_modules/**', '_book', 'styles', '_layouts', '_layouts/website', 'img' ],
       nosort: true
     }
   )
